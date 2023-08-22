409a4891d3dd2f3a2cdb1aa9b840cda1a1c28a9d
FabioGodoyy <fabiogodoy@Fabio>
2023-08-22 16:43:42 -0300

    vai meu filho

---
+++ app/components/analytics.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
+// import { DataChart } from "@react-google-analytics/google-analytics";
+
+export const Analitycs = () => {
+ 
+  return (
+    <>
+      <h1>Analytics</h1>
+      {/* Renderizar os elementos onde os gráficos serão exibidos */}
+      {/* <div id="view-selector-container" style={{ display: "none" }}></div>
+      <div id="date-range-selector-container" style={{ display: "none" }}></div>
+      <div id="visits-chart-container"></div>
+      <div id="sources-chart-container"></div>
+      <div id="pages-table-container"></div> */}
+    </>
+  );
+};
